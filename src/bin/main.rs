74b297881b02b22e09c2c9782179a1ca5d109c5d
tekerinkin <dmshestakov@icloud.com>
2022-08-10 12:20:08 +0300

    First commit

---
+++ src/bin/main.rs
@@ -0,0 +1,40 @@
+use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::fs;
+use ConcurrentServer::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        })
+    }
+
+    println!("Shutting down");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 НЕ НАЙДЕНО\r\n\r\n", "404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, content);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
